1974f99402431cb4e2b5547b55204a14a698480b
Lam Bui Bao <lambuibao@outlook.com>
2020-10-21 15:55:03 +0700

    Still writing

--- Arch/arch-vietnamese.sh
+++ Arch/arch-vietnamese.sh
@@ -4,5 +4,5 @@ git clone https://aur.archlinux.org/ibus-bamboo.git
 cd ibus-bamboo
 makepkg -si
 cd
-rm -r ibus-bamboo
+rm -rf ibus-bamboo
 echo "Please check your autostart settings and add in this command `ibus-daemon -drx`. This will autostart your Ibus daemon and then you should change your Ibus settings to have ibus-bamboo enabled."

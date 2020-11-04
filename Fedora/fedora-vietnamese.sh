581184e64edcb20e606910279f3978ed0a5ae35f
Lam Bui Bao <lambuibao@outlook.com>
2020-11-04 22:25:00 +0700

    .

--- Fedora/fedora-vietnamese.sh
+++ Fedora/fedora-vietnamese.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo dnf install ibus -y
-sudo dnf install https://download.opensuse.org/repositories/home:/lamlng/Fedora_32/x86_64/ibus-bamboo-0.6.7-1.1.x86_64.rpm -y
-
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_32/home:lamlng.repo
+sudo dnf install ibus-bamboo -y
--- script.rb
+++ script.rb
@@ -50,7 +50,7 @@ answer = gets.chomp
             elsif answer_fedora == "3"
                 `sh ./Fedora/fedora-vietnamese.sh`
                 output = `echo ?$`
-                if output = 0
+                if output == 0
                     puts "Looks like your installation went fine."
                 elsif output != 0
                     puts "Are you using Fedora 33? If so, there has been no builds done on OBS yet so we're waiting for that." 

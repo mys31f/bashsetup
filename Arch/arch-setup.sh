1a22400e587214da91cf9c51825f1f0329a28535
Sasha Scarlett <74601193+LexxFade@users.noreply.github.com>
2021-01-21 23:47:25 +0300

    why not get rid of removed pkg's dependencies
    pacman -Rns gets rid of all the unique dependencies these packages required; aka it cleans bloat.

--- Arch/arch-setup.sh
+++ Arch/arch-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo pacman -R firefox libreoffice-fresh --noconfirm
+sudo pacman -Rns firefox libreoffice-fresh --noconfirm
 sudo pacman -Syu --noconfirm
-sudo pacman -S  discord lutris steam kdenlive gimp krita inkscape wine neofetch qbittorrent
+sudo pacman -S discord lutris steam kdenlive gimp krita inkscape wine neofetch qbittorrent
 git clone 
 
 # Unfinished for now, will come back
